efdddbfb814c4b18cd87ddde989521ad3c785e0f
M16 <55531359+sahilrl@users.noreply.github.com>
2020-09-26 20:03:37 +0700

    Add files via upload

--- automorphic.py
+++ automorphic.py
@@ -1,7 +1,32 @@
-while (True):
+def automorphic(num, sq, cube, i):
+    while(i > 0):
+        if(num % 10 != sq % 10):
+            return False
+        else:
+            num //= 10
+            sq //= 10
+            i -= 1
+            return True
+
+
+def trimorphic(num, cube):
+    j = len(str(num))
+    while(j > 0):
+        if (num % 10 != cube % 10):
+            return False
+
+        else:
+            num //= 10
+            cube //= 10
+            j -= 1
+    return True
+
+
+def rerun():
+    while (True):
         try:
             print("Please enter a number: ")
-            num=int(input())
+            num = int(input())
 
         except ValueError:
             print("Not an integer. try again...")
@@ -9,39 +34,34 @@ while (True):
         else:
             break
 
-print('the input number is: '+str(num))
-sq=num*num
-cube=num*num*num
-i=len(str(num))
-def automorphic(num,sq,cube,i):
-    while(i>0):
-      if(num % 10 != sq % 10):
-          return False
-          break
-      else:
-          num //= 10
-          sq //= 10
-          i-=1
-    return True
-if automorphic(num,sq,cube,i):
-    print("Automorphic and Trimorphic both")
-else:
-    def trimorphic(num,cube):
-        j=len(str(num))
-        while(j>0):
-            if (num % 10 != cube % 10):
-                return False
-                break
-            else:
-                num //= 10
-                cube //= 10
-                j-=1
-        return True
-    if trimorphic(num,cube):
-        print("It's not Automorphic but its Trimorphic")
+    print('the input number is: '+str(num))
+    sq = num*num
+    cube = num*num*num
+    i = len(str(num))
+
+    if automorphic(num, sq, cube, i):
+        print("Automorphic and Trimorphic both")
     else:
-        print("It's neither Automorphic nor trimorphic")
+        if trimorphic(num, cube):
+            print("It's not Automorphic but its Trimorphic")
+        else:
+            print("It's neither Automorphic nor trimorphic")
 
 
+rerun()
 
-#check on 34(neither trimorphic nor automorphic), 24(Not amorphic but trimorphic), 26(Neither trimorphic not automorphic), 25(both automorphic and trimorphic)
+
+while(True):
+    print("Do you want to try another number?(y/n) ")
+    repeat = input()
+    if (repeat == 'y'):
+        rerun()
+    else:
+        print('Goodbye!')
+        break
+
+
+# On value 34 (neither trimorphic nor automorphic),
+# On value 24 (Not amorphic but trimorphic),
+# On value 26 (Neither trimorphic not automorphic),
+# On value 25 (both automorphic and trimorphic)
